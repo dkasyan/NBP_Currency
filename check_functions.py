7262d6cb680cc8840fbc282aaf788b0677a3dd7c
Damian Kasyan <damian.kasyan@hotmail.com>
2022-05-20 14:03:20 +0200

    check modules

---
+++ check_functions.py
@@ -0,0 +1,15 @@
+def who_are_you():
+    users = ("Damian", "Gaba")
+    newcomer = input("Kim jesteś? \n")
+    useregzist = False
+
+    for item in users:
+        while useregzist == False:
+            if item == newcomer:
+              print("Poprawnie")
+              useregzist = True
+            else:
+               newcomer = input("Zle! Kim jests \n")
+                
+
+who_are_you()
